468280dbb1e91257171d2700564e490a25e5f090
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2023-01-19 16:19:32 +0330

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./styles/input.scss";
+import "./scripts/themeSwitch.ts";
+import "./scripts/humburger.ts";
+import "../node_modules/@material-tailwind/html/scripts/toggle.js";
+import "../node_modules/@material-tailwind/html/scripts/tabs.js";
+import "../node_modules/@material-tailwind/html/scripts/collapse.js";
+import "../node_modules/@material-tailwind/html/scripts/dialog.js";
+import "../node_modules/@material-tailwind/html/scripts/input.js";
+import "../node_modules/@material-tailwind/html/scripts/menu.js"
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
+    darkMode:'class',
+    content: ["./src/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        // screens: {
+        //     sm:'480px',
+        //     md:'768px',
+        //     lg:'976px',
+        //     xl:'1440px'
+        // },
+        extend: {
+            colors:{
+                myColor1:"#e2e"
+            },
+            screen: {
+                esm:'400px'
+            },
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+              },
+            
+        },
+    },
+    plugins: [
+        require('tailwind-css-logical-properties')({})
+    ],
+})
